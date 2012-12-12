df85f647a03fdaf83362627c38c4d6a70ef4a3b2
Chris Continanza <christopher.continanza@gmail.com>
2012-12-11 19:29:34 -0800

    empty start test

---
+++ test/start_test.rb
@@ -0,0 +1,20 @@
+require 'helper'
+
+class StartTest < CheckCheckIt::TestCase
+
+  def setup
+    Examples.create_grocery_list(home)
+  end
+
+  def test_list_parses_steps
+    console.in_stream = MiniTest::Mock.new
+    3.times { console.in_stream.expect :gets, "y" }
+    result = check "start groceries"
+    console.in_stream.verify
+  end
+
+  def test_list_parses_commands
+  end
+
+end
+
